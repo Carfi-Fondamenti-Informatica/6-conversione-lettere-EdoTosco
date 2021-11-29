dd0ba0a316d872867f16937fc3953209564aca82
Edoardo Toscano <91884355+EdoTosco@users.noreply.github.com>
2021-11-29 10:40:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "CharaCheck.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char lettera;
+
+    cin>>lettera;
+
+    char output = characheck(lettera);
+
+    if(output=='7'){
+        cout<<"errore"<<endl;
+    }else{
+        cout << output << endl;
+    }
+
+return 0;
 }
