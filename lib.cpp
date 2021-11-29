813650ac932478f3ba128f87803870489dc0c441
Edoardo Toscano <91884355+EdoTosco@users.noreply.github.com>
2021-11-29 10:39:36 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,25 @@
+#include "CharaCheck.h"
 
+char characheck(char input){
+
+    bool flagcharacheck;
+    char convert;
+
+    if(((65<=int(input)) && (int(input)<=90)) || ((97<=int(input)) && (int(input)<=122))){
+        flagcharacheck = true;
+
+    }else{
+        flagcharacheck = false;
+    }
+
+    if(flagcharacheck==true && ((65<=int(input)) && (int(input)<=90))){
+        convert = int(input) + 32;
+
+    }else if(flagcharacheck==true && ((97<=int(input)) && (int(input)<=122))){
+        convert = int(input) - 32;
+
+    }else{
+        convert = '7';
+    }
+    return convert;
+}
